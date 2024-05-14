2a84968c755760e3f08e90ca25b905b99da89b54
Osayanny <osayanni7@gmail.com>
2024-05-14 17:25:18 +0500

    restructuring logic

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,28 +1,11 @@
 #!usr/bin/env python3
-
-from brain_games.cli import welcome_user
-from brain_games.even import generate_number, is_even
+from brain_games.engine.engine import engine
+from brain_games.games.even import even_logic
 
 
 def main():
-    print('Welcome to the Brain Games')
-    name = welcome_user()
-    print('Answer "yes" if the number is even, otherwise answer "no"')
-    correct = 0
-    while correct != 3:
-        number = generate_number()
-        print(f'Question: {number}')
-        answer = input('Answer: ')
-        correct_answer = is_even(number)
-        if correct_answer == answer:
-            print('Correct!')
-            correct += 1
-        else:
-            print(f'"{answer}" is wrong answer ;(. Correct answer was "{correct_answer}"')
-            print(f"Let's try again, {name}!")
-            break
-    else:
-        print(f'Congratultions, {name}!')
+    discription = 'Answer "yes" if the number is even, otherwise answer "no".'
+    engine(discription, even_logic)
 
 
 if __name__ == '__main__':
