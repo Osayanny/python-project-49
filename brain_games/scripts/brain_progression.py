9b4c8f359aa9f1a3e702f063096b8eb9c3646d12
Osayanny <osayanni7@gmail.com>
2024-05-16 15:08:31 +0500

    Add script for new progression game

---
+++ brain_games/scripts/brain_progression.py
@@ -0,0 +1,12 @@
+#!usr/bin/env python3
+from brain_games.engine.engine import engine
+from brain_games.games.progression import progression_logic
+
+
+def main():
+    discription = 'What number is missing in the progression?'
+    engine(discription, progression_logic)
+
+
+if __name__ == '__main__':
+    main()
