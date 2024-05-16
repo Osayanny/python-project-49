b5385207897ec9d86a0969fcaec9a566dd594678
Osayanny <osayanni7@gmail.com>
2024-05-16 15:10:03 +0500

    Minor fixes

--- brain_games/engine/special_funcs.py
+++ brain_games/engine/special_funcs.py
@@ -3,6 +3,7 @@ import random
 # Uneversal functions
 
 
-def generate_number():
-    number = random.randint(1, 100)
+def generate_number(gen_range=(1, 100)):
+    first_bord, second_bord = gen_range
+    number = random.randint(first_bord, second_bord)
     return number
