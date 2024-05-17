bec92da986b655b30d4d05dabb18ecd8e4717133
Osayanny <osayanni7@gmail.com>
2024-05-17 14:40:58 +0500

    Add logic for "Prime or not" game

---
+++ brain_games/games/prime.py
@@ -0,0 +1,19 @@
+from brain_games.engine.special_funcs import generate_number
+
+
+def is_prime(num):
+    divisiors = [_ for _ in range(1, num+1)]
+    divisiors_count = 0
+    for i in divisiors:
+        if divisiors_count < 2:
+            if num % i == 0:
+                divisiors_count += 1
+        else:
+            return 'no'
+    return 'yes'
+
+
+def prime_logic():
+    question = generate_number()
+    answer = is_prime(question)
+    return question, answer
