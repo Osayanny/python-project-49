9493caca0c29847fac16c6fab2cb1ed0b53181b4
Osayanny <osayanni7@gmail.com>
2024-05-20 14:46:37 +0500

    Remove useless module

--- brain_games/engine/special_funcs.py
+++
@@ -1,9 +0,0 @@
-import random
-
-# Uneversal functions
-
-
-def generate_number(gen_range=(1, 100)):
-    first_bord, second_bord = gen_range
-    number = random.randint(first_bord, second_bord)
-    return number
