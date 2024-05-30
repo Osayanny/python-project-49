113a0abe82c43310a1de7e6eff2e15cd43749e0c
Osayanny <osayanni7@gmail.com>
2024-05-30 15:31:54 +0500

    edit correct answer check

--- brain_games/engine/engine.py
+++ brain_games/engine/engine.py
@@ -2,20 +2,19 @@ import prompt
 from brain_games.engine.cli import welcome_user
 
 
-ROUNDS = 3
+_ROUNDS_COUNT = 3
 
 
 def start_engine(game):
     name = welcome_user()
     print(f'{game.DESCRIPTION}')
-    correct = 0
-    while correct != ROUNDS:
+
+    for _ in range(0, _ROUNDS_COUNT):
         question, game_answer = game.get_question_and_answer()
         print(f'Question: {question}')
         user_answer = prompt.string('Answer: ')
-        if user_answer == str(game_answer):
+        if user_answer == game_answer:
             print('Correct!')
-            correct += 1
         else:
             print(f"'{user_answer}' is wrong answer ;(", end=' ')
             print(f"Correct answer was '{game_answer}'")
